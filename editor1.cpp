32e8aeb5576138f354817844361b5ee90a8220ef
Jeff Lawson <jeff@bovine.net>
2017-12-09 07:32:39 +0000

    relicense under BSD-style. Update contact information.

--- editor1.cpp
+++ editor1.cpp
@@ -1,41 +1,7 @@
-// []-----------------------------------------------------------------------[]
-// |                                                                         |
-// |                                                                         |
-// |  This source and its associated executable are copyright by Jeff Lawson |
-// |  of JL Enterprises, however some portions of this code may or may not   |
-// |  have been originally inspired by other authors and credit is given if  |
-// |  due.  You are encouraged to redistribute this package, but you must    |
-// |  include all files originally distributed in this archive in their      |
-// |  original state and format and without modifications of any kind.       |
-// |  Under no circumstances may you make modifications to this product and  |
-// |  redistribute the resulting code or executables.  If you believe you    |
-// |  have made some useful or otherwise important enhancements and you      |
-// |  would like to share these changes with others, please contact the      |
-// |  author through one of the methods listed below.  Additionally, no      |
-// |  fees may be charged for the usage of this product by anyone other than |
-// |  the author of this file, except for fees (not to exceed US $10) by     |
-// |  disk distributors to cover disk duplication and handling.  If there    |
-// |  is any need to question this policy, please contact the author.        |
-// |  This source code and its executable are distributed without any kind   |
-// |  of warranty and the author may not be held accountable for damages of  |
-// |  any kind.                                                              |
-// |                                                                         |
-// |                                                                         |
-// |  I can reached via postal mail at:                                      |
-// |                                                                         |
-// |          Jeff Lawson                                                    |
-// |          1893 Kaweah Drive                                              |
-// |          Pasadena, CA 91105-2174                                        |
-// |          USA                                                            |
-// |                                                                         |
-// |  or through E-mail at:                                                  |
-// |                                                                         |
-// |          JeffLawson@Aol.Com                                             |
-// |                                                                         |
-// |  also via phone at:                                                     |
-// |                                                                         |
-// |          (213) 258-5604                                                 |
-// []-----------------------------------------------------------------------[]
+// Xcom2edi
+// Copyright (c) 1997, Jeff Lawson <jeff@bovine.net>
+// All rights reserved.
+// See LICENSE for distribution information.
 
 #include "editor.h"
 
@@ -315,15 +281,9 @@ static int main_screen (void)
                 "This specific version was compiled on\n"
                 __DATE__ " at " __TIME__ ".  If you\n"
                 "want to talk to the author, you may\n"
-                "write me at \2jlawson@hmc.edu\1 or \n"
-                "\2JeffLawson@aol.com\1 or at the\n"
-                "following postal address:\n\n"
-                "    \2Jeff Lawson\n"
-                "    1893 Kaweah Drive\n"
-                "    Pasadena, CA 91105-2174\n"
-                "    USA\1\n\n"
+                "write me at \2jeff@bovine.net\1\n"
                 "I can also be reached via the WWW at\n"
-                "\2http://members.aol.com/JeffLawson/\1\n"
+                "\2http://www.bovine.net/\1\n"
                 );
             buttontype ok_button(COLOR(BLACK, LIGHT_WHITE), COLOR(LIGHT_WHITE, GRAY),\
                 ABOUT_ROW + 19, ABOUT_COL + 17, "OK");
